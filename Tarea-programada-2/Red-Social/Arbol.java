d30852de64da998f7f3678ef47961c69c04b4f44
KyaraMR <kyara.munoz@ucr.ac.cr>
2021-11-27 14:13:27 -0600

    Creación de la clase Arbol

---
+++ Tarea-programada-2/Red-Social/Arbol.java
@@ -0,0 +1,84 @@
+
+/**
+ * Write a description of class Arbol here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Arbol
+{
+    private NodoArbol raiz;
+    
+    /**
+     * Descripción
+     * 
+     * @param
+     * @return
+     */
+    public Arbol() {
+        raiz = null;
+    }
+    
+    /**
+     * Descripción
+     * 
+     * @param
+     * @return
+     */
+    public NodoArbol getRaiz() {
+        return raiz;
+    }
+    
+    /**
+     * Descripción
+     * 
+     * @param
+     * @return
+     */
+    public void setRaiz(NodoArbol raiz) {
+        this.raiz = raiz;
+    }
+    
+    /**
+     * Verifica si el nombre digitado por el usuario se encuentra dentro de la lista.
+     * 
+     * @param nombre Nombre de la persona a buscar en la lista que digita el usuario.
+     * @return true si el nombre digitado se encuentra dentro de la lista o false en caso contrario.
+     */
+    public boolean verificarExistencia(String nombre) {
+        boolean existe = false;
+        
+        if (raiz != null) {
+            if (raiz.getPersona().getNombre().equals(nombre)) {
+                existe = true;
+            } else {
+                if (nombre.compareTo(raiz.getPersona().getNombre()) < 0) {
+                    existe = raiz.getArbolIzquierdo().verificarExistencia(nombre);
+                } else {
+                    existe = raiz.getArbolDerecho().verificarExistencia(nombre);
+                }
+            }
+        }
+        
+        return existe;
+    }
+    
+    public boolean agregarAmigo(Persona nuevoAmigo) {
+        boolean agregado = true;
+        
+        if (verificarExistencia(nuevoAmigo.getNombre()) == false) {
+            if (raiz == null) {
+                NodoArbol nodoNuevo = new NodoArbol(nuevoAmigo);
+                raiz = nodoNuevo;
+            } else {
+                if (nuevoAmigo.getNombre().compareTo(raiz.getPersona().getNombre()) < 0) {
+                    raiz.getArbolIzquierdo().agregarAmigo(nuevoAmigo);
+                } else {
+                    raiz.getArbolDerecho().agregarAmigo(nuevoAmigo);
+                }
+            }
+        }
+        
+        return agregado;
+    }
+}
