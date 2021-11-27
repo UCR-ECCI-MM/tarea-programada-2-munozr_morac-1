f6c08670dc0c95a58e1c7548fd55bbc831d41446
JosePabloMoraC <84686084+JosePabloMoraC@users.noreply.github.com>
2021-11-27 17:09:10 -0600

    Revertir

--- Tarea-programada-2/Red-Social/Arbol.java
+++ Tarea-programada-2/Red-Social/Arbol.java
@@ -59,6 +59,7 @@ public class Arbol
         return resultado;
     }
     
+<<<<<<< HEAD
         public Persona buscarMenor(){
         Persona menor = null;
         
@@ -72,6 +73,8 @@ public class Arbol
     }
     
  
+=======
+>>>>>>> parent of a7a9efa (toString de la clase Arbol)
     
     
     
